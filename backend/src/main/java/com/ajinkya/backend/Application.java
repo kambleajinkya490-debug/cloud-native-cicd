6898bcf092737284e7a4a08237a59f2911663824
root <root@ip-172-31-3-195.ap-south-1.compute.internal>
2026-08-24 14:56:44 +0000

    Add Spring Boot backend

---
+++ backend/src/main/java/com/ajinkya/backend/Application.java
@@ -0,0 +1,19 @@
+package com.ajinkya.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Cloud Native CI/CD on AWS is Running!";
+    }
+}
